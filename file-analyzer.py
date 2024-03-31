38c572894711610fe354a57a14953f2b069e3186
rvuong28 <126596097+rvuong28@users.noreply.github.com>
2024-03-30 21:15:27 -0400

    Update file-analyzer.py
    Added function to upload file before analyzing report. Will have the 2 options to print "File Analysis" (smaller description of the report of the file), or a "File Report" (a more detailed description of the report of the file).

--- file-analyzer.py
+++ file-analyzer.py
@@ -18,23 +18,52 @@ def get_hash(file):
             file_bytes = open_file.read(READ_SIZE)
     return file_hash.hexdigest()
 
-# Run program like "python3 file-analyzer.py <file>"
-def main():
-    file = sys.argv[1]
-    file_hash = get_hash(file)
+#Uploading File to VirusTotal
+def uploadFile(fileName):
+    url = "https://www.virustotal.com/api/v3/files"
+    files = {"file": open(fileName, "rb")}
+    headers =  {"accept": "application/json", 'x-apikey': API_KEY}
+    response = requests.post(url, headers=headers, files=files)
+    if response.headers['content-type'] == 'application/json':
+        return response.json()['data']['id']
 
-    print("Hash: " + file_hash + "\n\n")
-
-    url = "https://www.virustotal.com/api/v3/files/" + file_hash
-
-    headers = {"accept": "application/json", 'x-apikey': ""}
-
+#Print Analysis of File
+def fileAnalysis(file_ID):
+    url = "https://www.virustotal.com/api/v3/analyses/" + file_ID
+    headers =  {"accept": "application/json", 'x-apikey': API_KEY}
     response = requests.get(url, headers=headers)
     print(response.text)
 
+#Retrieving file report of the hashed file
+def retrieveReport(file):
+
+    file_hash = get_hash(file)
+    url = "https://www.virustotal.com/api/v3/files/" + file_hash
+    headers = {"accept": "application/json", 'x-apikey': API_KEY}
+    response = requests.get(url, headers=headers)
+    print(response.text)
+
+
+# Run program like "python3 file-analyzer.py <file>"
+def main():
+    #Saving File
+    file = sys.argv[1]
+
+    #Uploads the file and returns the file ID (so it can be used for a file analysis)
+    file_id = uploadFile(file)
+
+    #Option to Print out a File Analysis or File Report
+    #1. File Analysis - prints information on the analysis object (using id from uploadFile)
+    #2. File Report - retrieve detailed information about a file
+    print('Enter Options "1" or "2": \n 1. File Analysis \n 2. File Report')
+    answer = input()
+    if answer == "1":
+        fileAnalysis(file_id)
+    elif answer == "2":
+        retrieveReport(file)
+
     #report = get_request(file_hash)
     #print(report)
-
 
 if __name__ == "__main__":
     main()
